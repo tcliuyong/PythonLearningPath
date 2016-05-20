57230a809c5e0d3c2965ef24003d4417df7d4d69
liuyong <583633876@qq.com>
2016-05-20 08:24:07 +0800

    sample

---
+++ com/python/pythontool/main.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import sys
+from pql import MysqlTools
+host="127.0.0.1"
+user="root"
+passwd="liuyong"
+db="testdb"
+port=3306
+if __name__=="__main__":
+    pql = MysqlTools.MysqlTools(host, user, passwd, db, port)
+    column_name = ("username","passwd")
+    #values = (("test1","ss"),("test2","ss"))
+    #pql.insertIntoDB("user",column_name, values)
+    records = pql.selectFromDB("select * from user")
+    print records
+    pql.insertIntoDBFromText("user", column_name,r"demo\\person.txt")
